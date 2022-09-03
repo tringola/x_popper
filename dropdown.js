0c416651fb68c8869e5d4696025912d617686b1a
vico <vicoparagon@gmail.com>
2022-09-03 13:11:00 +0200

    initi_app tooltip dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,22 +1,94 @@
 /**
  * Created by tringola on 30/08/2022.
  */
-function Accordion(target){
-
+//create popper instance
+function createInstance(starter,template,type) {
+let create= {
+    destroyInstance : function(popperInstance) {
+        if (popperInstance) {
+            popperInstance.destroy();
+            popperInstance = null;
+        }
+    },
+    showPopper : function(popperPopup) {
+        popperPopup.setAttribute("show-popper", "");
+        //popperArrow.setAttribute("data-popper-arrow", "");
+        createInstance();
+    },
+    hidePopper : function(popperPopup) {
+        popperPopup.removeAttribute("show-popper");
+      //  popperArrow.removeAttribute("data-popper-arrow");
+        create.destroyInstance();
+    },
+    togglePopper : function(popperPopup) {
+        if (popperPopup.hasAttribute("show-popper")) {
+            create.hidePopper();
+        } else {
+            create.showPopper();
+        }
+    }
+};
+    let popup = document.createElement("DIV");
+    popup.innerHTML = template.innerHTML;
+    popup.setAttribute('role', 'dropdown');
+    popup.classList.add('tooltip');
+    popup.style.display ="none";
+    document.body.appendChild(popup);
+    starter.addEventListener('click', function(){
+        create.togglePopper(popup);
+    });
+    let popperInstance = new Popper(starter, popup, {
+        placement: "auto", //preferred placement of popper
+        modifiers: [
+            {
+                name: "offset", //offsets popper from the reference/button
+                options: {
+                    offset: [0, 8]
+                }
+            },
+            {
+                name: "flip", //flips popper with allowed placements
+                options: {
+                    allowedAutoPlacements: ["right", "left", "top", "bottom"],
+                    rootBoundary: "viewport"
+                }
+            }
+        ]
+    });
 }
 
-const accItems = document.querySelectorAll(".accordion__item");
-
-// add a click event for all items
-accItems.forEach(function(acc){acc.addEventListener("click", toggleAcc)} );
-
-function toggleAcc() {
-    // remove active class from all items exept the current item (this)
-    accItems.forEach(function(item){item !== this ? item.classList.remove("accordion__item--active") : null});
-
-
-    // toggle active class on current item
-    if (!this.classList.contains("accordion__item--active")) {
-        this.classList.toggle("accordion__item--active");
+//destroy popper instance
+function destroyInstance() {
+    if (popperInstance) {
+        popperInstance.destroy();
+        popperInstance = null;
     }
 }
+
+//show and create popper
+function showPopper() {
+    popperPopup.setAttribute("show-popper", "");
+    popperArrow.setAttribute("data-popper-arrow", "");
+    createInstance();
+}
+
+//hide and destroy popper instance
+function hidePopper() {
+    popperPopup.removeAttribute("show-popper");
+    popperArrow.removeAttribute("data-popper-arrow");
+    destroyInstance();
+}
+
+//toggle show-popper attribute on popper to hide or show it with CSS
+function togglePopper() {
+    if (popperPopup.hasAttribute("show-popper")) {
+        hidePopper();
+    } else {
+        showPopper();
+    }
+}
+//execute togglePopper function when clicking the popper reference/button
+/* popperButton.addEventListener("click", function (e) {
+ e.preventDefault();
+ togglePopper();
+ });*/
