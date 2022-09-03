5750004c1c9cd1e320fcd8d2357e00095b3dd14d
vico <vicoparagon@gmail.com>
2022-09-03 14:30:03 +0200

    create function for create objetc wich create popup object

--- dropdown.js
+++ dropdown.js
@@ -2,42 +2,44 @@
  * Created by tringola on 30/08/2022.
  */
 //create popper instance
-function createInstance(starter,template,type) {
-let create= {
-    destroyInstance : function(popperInstance) {
-        if (popperInstance) {
-            popperInstance.destroy();
-            popperInstance = null;
-        }
-    },
-    showPopper : function(popperPopup) {
-        popperPopup.setAttribute("show-popper", "");
-        //popperArrow.setAttribute("data-popper-arrow", "");
-        createInstance();
-    },
-    hidePopper : function(popperPopup) {
-        popperPopup.removeAttribute("show-popper");
-      //  popperArrow.removeAttribute("data-popper-arrow");
-        create.destroyInstance();
-    },
-    togglePopper : function(popperPopup) {
-        if (popperPopup.hasAttribute("show-popper")) {
-            create.hidePopper();
-        } else {
-            create.showPopper();
-        }
+function PopupInstance(starter,template,popupArrow) {
+    var self = this;
+    this.popup = document.createElement("DIV");
+    this.popup.innerHTML = template.innerHTML;
+    this.popup.setAttribute('role', 'dropdown');
+    this.popup.classList.add('tooltip');
+    this.popup.style.display ="none";
+    //this.popup.setAttribute("show-popper", "");
+    this.popupArrow = popupArrow;
+    document.body.appendChild(self.popup);
+    this.popperInstance = false;
+    this.starter =starter;
+    starter.addEventListener("click",function(){
+        self.togglePopper();
+    })
+}
+
+//destroy popper instance
+PopupInstance.prototype.destroyInstance = function() {
+    "use strict";
+    var self =this;
+    if (self.popperInstance) {
+        self.popperInstance.destroy();
+        self.popperInstance = null;
     }
 };
-    let popup = document.createElement("DIV");
-    popup.innerHTML = template.innerHTML;
-    popup.setAttribute('role', 'dropdown');
-    popup.classList.add('tooltip');
-    popup.style.display ="none";
-    document.body.appendChild(popup);
-    starter.addEventListener('click', function(){
-        create.togglePopper(popup);
-    });
-    let popperInstance = new Popper(starter, popup, {
+
+//show and create popper
+PopupInstance.prototype.showPopper = function() {
+    "use strict";
+    var self =this;
+    if(self.popupArrow!==undefined){
+        self.popupArrow.setAttribute("data-popper-arrow", "");
+    }
+    self.popup.setAttribute("show-popper", "");
+
+    self.popup.style.display ="inline-block";
+    self.popperInstance = new Popper(self.starter, self.popup, {
         placement: "auto", //preferred placement of popper
         modifiers: [
             {
@@ -55,38 +57,30 @@ let create= {
             }
         ]
     });
-}
-
-//destroy popper instance
-function destroyInstance() {
-    if (popperInstance) {
-        popperInstance.destroy();
-        popperInstance = null;
-    }
-}
-
-//show and create popper
-function showPopper() {
-    popperPopup.setAttribute("show-popper", "");
-    popperArrow.setAttribute("data-popper-arrow", "");
-    createInstance();
-}
+};
 
 //hide and destroy popper instance
-function hidePopper() {
-    popperPopup.removeAttribute("show-popper");
-    popperArrow.removeAttribute("data-popper-arrow");
-    destroyInstance();
-}
+PopupInstance.prototype.hidePopper = function( ) {
+    "use strict";
+    var self =this;
+    self.popup.removeAttribute("show-popper");
+    self.popup.style.display ="none";
+    if(self.popupArrow!==undefined){
+        self.popupArrow.removeAttribute("data-popper-arrow");
+    }
+    self.destroyInstance();
+};
 
 //toggle show-popper attribute on popper to hide or show it with CSS
-function togglePopper() {
-    if (popperPopup.hasAttribute("show-popper")) {
-        hidePopper();
+PopupInstance.prototype.togglePopper = function() {
+    "use strict";
+    var self =this;
+    if (self.popup.hasAttribute("show-popper")) {
+        self.hidePopper();
     } else {
-        showPopper();
+        self.showPopper();
     }
-}
+};
 //execute togglePopper function when clicking the popper reference/button
 /* popperButton.addEventListener("click", function (e) {
  e.preventDefault();
