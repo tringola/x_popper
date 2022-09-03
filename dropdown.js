452a8f5a0c6ee4f663465c4fbc261205852d4f92
vico <vicoparagon@gmail.com>
2022-09-03 16:42:57 +0200

    add parameter for init popper(new instance in bottom, top, or auto('tooltip:top, dropdown:bottom, popup:auto'

--- dropdown.js
+++ dropdown.js
@@ -2,20 +2,26 @@
  * Created by tringola on 30/08/2022.
  */
 //create popper instance
-function PopupInstance(starter,template,popupArrow) {
+function PopupInstance(starter,template,type, event) {
     var self = this;
+    self.type = type;
     this.popup = document.createElement("DIV");
     this.popup.innerHTML = template.innerHTML;
     this.popup.setAttribute('role', 'dropdown');
     this.popup.classList.add('tooltip');
     this.popup.style.display ="none";
     //this.popup.setAttribute("show-popper", "");
-    this.popupArrow = popupArrow;
     document.body.appendChild(self.popup);
     this.popperInstance = false;
     this.starter =starter;
-    starter.addEventListener("click",function(){
-        self.togglePopper();
+    document.addEventListener("click",function(e){
+        if(e.target===starter){
+            self.togglePopper();
+        }
+        else if(e.target!==starter && e.target!==self.popup){
+            self.hidePopper();
+        }
+
     })
 
 }
@@ -34,30 +40,51 @@ PopupInstance.prototype.destroyInstance = function() {
 PopupInstance.prototype.showPopper = function() {
     "use strict";
     var self =this;
-    if(self.popupArrow!==undefined){
-        self.popupArrow.setAttribute("data-popper-arrow", "");
-    }
+
     self.popup.setAttribute("show-popper", "");
 
     self.popup.style.display ="inline-block";
-    self.popperInstance = new Popper(self.starter, self.popup, {
-        placement: "auto", //preferred placement of popper
-        modifiers: [
-            {
-                name: "offset", //offsets popper from the reference/button
-                options: {
-                    offset: [0, 8]
-                }
-            },
-            {
-                name: "flip", //flips popper with allowed placements
-                options: {
-                    allowedAutoPlacements: ["right", "left", "top", "bottom"],
-                    rootBoundary: "viewport"
+var modifier = {};
+var placement="";
+
+    if(self.type==='dropdown'){
+        placement = "bottom";
+        modifiers = {
+            offset:  {
+                enable:true,
+                    offset: '0,8'
+            }
+        };
+    }
+    else if(self.type==='tooltip'){
+        placement = "top";
+            modifiers= {
+
+                offset:  {
+                    enable:true,
+                    offset: '0,8'
                 }
             }
-        ]
+    }
+
+    else if(self.type==='popup'){
+        placement = "auto";
+            modifiers= {
+                offset:  {
+                    enable:true,
+                    offset: '0,8'
+                },
+                flip:{
+                    behavior:[ "left", "right","top", "bottom"]
+                }
+            }
+    }
+
+    self.popperInstance = new Popper(self.starter, self.popup, {
+        placement: placement, //preferred placement of popper
+        modifiers
     });
+
 };
 
 //hide and destroy popper instance
@@ -66,9 +93,7 @@ PopupInstance.prototype.hidePopper = function( ) {
     var self =this;
     self.popup.removeAttribute("show-popper");
     self.popup.style.display ="none";
-    if(self.popupArrow!==undefined){
-        self.popupArrow.removeAttribute("data-popper-arrow");
-    }
+
     self.destroyInstance();
 };
 
